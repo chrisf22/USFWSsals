ce1f3c2a63f041d1a6626747b727d0ac4c8c2c76
Chris Field <chrisfield22@gmail.com>
2020-01-05 10:59:31 -0500

    Code to interpolate the LSL projection for NYC from Kopp et al. 2014.

---
+++ USFWSsals_Koppetal_SLR.R
@@ -0,0 +1,30 @@
+
+NYC_LSL_8.5 <- rep(NA, 101)
+NYC_LSL_8.5[1] <- 0
+NYC_LSL_8.5[31] <- 20
+NYC_LSL_8.5[51] <- 38
+NYC_LSL_8.5[101] <- 96
+
+NYC_LSL_4.5 <- rep(NA, 101)
+NYC_LSL_4.5[1] <- 0
+NYC_LSL_4.5[31] <- 20
+NYC_LSL_4.5[51] <- 36
+NYC_LSL_4.5[101] <- 75
+
+NYC_LSL_2.6 <- rep(NA, 101)
+NYC_LSL_2.6[1] <- 0
+NYC_LSL_2.6[31] <- 20
+NYC_LSL_2.6[51] <- 35
+NYC_LSL_2.6[101] <- 62
+
+x <- 1:101
+plot(predict(loess(NYC_LSL_8.5 ~ x, span=1.5), x))
+NYC_LSL_8.5_int <- predict(loess(NYC_LSL_8.5 ~ x, span=1.5), x)
+
+plot(predict(loess(NYC_LSL_4.5 ~ x, span=1.5), x))
+NYC_LSL_4.5_int <- predict(loess(NYC_LSL_4.5 ~ x, span=1.5), x)
+
+plot(predict(loess(NYC_LSL_2.6 ~ x, span=1.5), x))
+NYC_LSL_2.6_int <- predict(loess(NYC_LSL_2.6 ~ x, span=1.5), x)
+
+cbind((x+1999), round(NYC_LSL_8.5_int), round(NYC_LSL_4.5_int), round(NYC_LSL_2.6_int))
