d777e7e6501522a576884d3cbb9911f465f4e56a
Chris Field <chrisfield22@gmail.com>
2020-03-23 16:58:40 -0400

    Added threshold slider and ability to select the variable for the y-axis.

--- USFWSsals_shiny_map_wplotly.R
+++ USFWSsals_shiny_map_wplotly.R
@@ -12,6 +12,13 @@ ui <- fluidPage(
   
   fluidRow(
     column(1,
+           selectInput(inputId = "ytest", 
+                       label = "Indicator to display on Y-axis", 
+                       choices = c("Forest loss" = "1", 
+                                   "Saltmarsh Sparrow abundance" = "2"), 
+                       selected = "Forest_loss"),
+          sliderInput("slider1", label = "Threshold", min = 0, 
+                                   max = 1, value = 0.20),
            radioButtons("state", "State",
                        c("ALL" = "1",
                          "VA" = "2",
@@ -25,6 +32,12 @@ ui <- fluidPage(
                          "NH" = "10",
                          "ME" = "11")
            )),
+    #column(1,
+           #selectInput(inputId = "ytest", 
+                       #label = "Indicator to display on Y-axis", 
+                       #choices = c("Forest loss" = "1", 
+                                   #"Saltmarsh Sparrow abundance" = "2"), 
+                       #selected = "Forest_loss")),
     column(5,
            plotOutput("plot2", click = "plot2_click", height = "600px"),
            helpText("Click on a point to see its location in the map window. Data for the mapped location is shown below the window. ")
@@ -37,12 +50,13 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  load("SHARP_patches_wforest.RData")
+  #load("SHARP_patches_wforest.RData")
   # to avoid warnings, make all zeros in SHARP_patches_4plot = NA
   SHARP_patches_4plot[SHARP_patches_4plot == 0] <- NA
   # 'SHARP_patches_4plot' must be in workspace
   colnames(SHARP_patches_4plot)[1] <- "SHARP_Patch_ID"
   colnames(SHARP_patches_4plot)[4] <- "Forest_loss"
+  colnames(SHARP_patches_4plot)[5] <- "Slope"
   colnames(SHARP_patches_4plot)[6] <- "Saltmarsh_Sparrow_abundance"
     
   output$click_info <- renderPrint({
@@ -65,14 +79,25 @@ server <- function(input, output, session) {
   bystate[,10] <- SHARP_patches_4plot$States == "NH"
   bystate[,11] <- SHARP_patches_4plot$States == "ME"
   
+  varnames <- c("Forest_loss", "Slope")
+  axisbounds <- list(c(0.0001, 0.001, 0.01, 0.1, 1), c(0.0001, 0.001, 0.01, 0.1, 1))
+  axislabels <- list(c('0.0001', '0.01', '1', '100', '10000'), c('0.0001', '0.01', '3', '100', '10000'))
+  ylabs <- c("Forest loss (poportion of patch)", "Slope")
+  
   # where does this go?
   output$plot2 <- renderPlot({
-    plot(SHARP_patches_4plot$Saltmarsh_Sparrow_abundance[as.logical(bystate[,as.numeric(input$state)])], SHARP_patches_4plot$Forest_loss[as.logical(bystate[,as.numeric(input$state)])], log="xy", xaxt="n", yaxt="n", 
-         xlab="Saltmarsh sparrow abundance", ylab="Forest loss (poportion of patch)", bty="n", pch=16, col=rgb(0, 0, 0, 0.5), cex=1.5, cex.lab=1.25)
+    plot(SHARP_patches_4plot$Saltmarsh_Sparrow_abundance[as.logical(bystate[,as.numeric(input$state)])], SHARP_patches_4plot[,varnames[as.numeric(input$ytest)]][as.logical(bystate[,as.numeric(input$state)])], log="xy", xaxt="n", yaxt="n", 
+         xlab="Saltmarsh sparrow abundance", ylab=ylabs[as.numeric(input$ytest)], bty="n", pch=16, col=rgb(0, 0, 0, 0.5), cex=1.5, cex.lab=1.25)
     axis(side=1, at=c(0.0001, 0.01, 1, 100, 10000), 
          labels=c('0.0001', '0.01', '1', '100', '10000'), cex.axis=1.25)
-    axis(side=2, at=c(0.0001, 0.001, 0.01, 0.1, 1), 
-         labels=c('0.0001', '0.001', '0.01', '0.1', '1'), cex.axis=1.25)
+    #axis(side=2, at=c(0.0001, 0.001, 0.01, 0.1, 1), 
+         #labels=c('0.0001', '0.001', '0.01', '0.1', '1'), cex.axis=1.25)
+    axis(side=2, at=axisbounds[[as.numeric(input$ytest)]], 
+    labels=axislabels[[as.numeric(input$ytest)]], cex.axis=1.25)
+    xx <- SHARP_patches_4plot$Saltmarsh_Sparrow_abundance[as.logical(bystate[,as.numeric(input$state)])] >= quantile(SHARP_patches_4plot$Saltmarsh_Sparrow_abundance[as.logical(bystate[,as.numeric(input$state)])], c(1-input$slider1), na.rm=TRUE)
+    yy <- SHARP_patches_4plot[,varnames[as.numeric(input$ytest)]][as.logical(bystate[,as.numeric(input$state)])] >= quantile(SHARP_patches_4plot[,varnames[as.numeric(input$ytest)]][as.logical(bystate[,as.numeric(input$state)])], c(1-input$slider1), na.rm=TRUE)
+    points(SHARP_patches_4plot$Saltmarsh_Sparrow_abundance[as.logical(bystate[,as.numeric(input$state)])][yy], SHARP_patches_4plot[,varnames[as.numeric(input$ytest)]][as.logical(bystate[,as.numeric(input$state)])][yy], pch=16, col="blue")
+    points(SHARP_patches_4plot$Saltmarsh_Sparrow_abundance[as.logical(bystate[,as.numeric(input$state)])][xx], SHARP_patches_4plot[,varnames[as.numeric(input$ytest)]][as.logical(bystate[,as.numeric(input$state)])][xx], pch=16, col=rgb(1, 0, 0, 0.5))
   })
   
   output$mymap <- renderLeaflet({
